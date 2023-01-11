14e641d895a7f85f18a778d0bfce2da9c8119bb0
abolodurin <shur026@yandex.ru>
2023-01-11 19:31:08 +0300

    corrections done

--- src/main/webapp/Lesson10/3_requests.sql
+++ src/main/webapp/Lesson10/3_requests.sql
@@ -1,17 +1,22 @@
-SELECT name, subject_name, mark FROM progress
-INNER JOIN students ON student_id = fk_student_id
-INNER JOIN subjects ON subject_id = fk_subject_id
-WHERE mark > 3 AND subject_id = 14;
+SELECT stu.student_name AS name, pro.mark
+FROM progress AS pro
+INNER JOIN students AS stu
+ON stu.student_id = pro.student_id
+INNER JOIN subjects AS subj
+ON subj.subject_id = pro.subject_id
+WHERE mark > 3 AND subj.subject_name = 'English';
 
-DELETE FROM students WHERE student_id = 38;
+DELETE FROM students WHERE student_id = 7;
 
 SELECT AVG (mark) FROM progress
-WHERE fk_subject_id = 13;
+WHERE subject_id = 3;
 
 SELECT AVG (mark) FROM progress
-WHERE fk_student_id = 34;
+WHERE student_id = 3;
 
-SELECT subject_name FROM subjects 
-INNER JOIN progress ON subject_id = fk_subject_id
-GROUP BY subject_name
-ORDER BY COUNT(mark) DESC LIMIT 3;
+SELECT subj.subject_name AS subject
+FROM subjects AS subj
+INNER JOIN progress AS pro
+ON subj.subject_id = pro.subject_id
+GROUP BY subject
+ORDER BY COUNT(DISTINCT (student_id, pro.subject_id)) DESC LIMIT 3;
